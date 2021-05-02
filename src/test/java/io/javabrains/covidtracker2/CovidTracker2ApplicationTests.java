128394437451af3bb118a30ecd86f3917a35791d
Syedraza <Syedraza2508@gmail.com>
2021-05-02 17:03:54 +0200

    test Reza

---
+++ src/test/java/io/javabrains/covidtracker2/CovidTracker2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.covidtracker2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTracker2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
